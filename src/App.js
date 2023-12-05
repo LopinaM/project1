375bdc0f216cc4908be2d500a551a164017c6d3a
Maria <mlopina@digital-upstream.ru>
2023-12-05 17:31:46 +0500

    dispatch

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ function App(props) {
             <Route path="profile"
               element={<Profile
                 profilePage={props.state.profilePage}
-                addPost={props.addPost}
-                updateNewPostText={props.updateNewPostText} />} />
+                dispatch={props.dispatch} />} />
 
             <Route path="dialogs"
               element={<Dialogs state={props.state.dialogsPage} />} />
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -11,13 +11,15 @@ function MyPosts(props) {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        // props.addPost();
         // props.updateNewPostText('');
+        props.dispatch({ type: 'ADD-POST' });
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
         console.log(text)
-        props.updateNewPostText(text);
+        // props.updateNewPostText(text);
+        props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text });
     }
 
     return (
