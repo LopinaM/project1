8b816b5ca9c4469039caf6315f482a4fc4147c5e
Maria <mlopina@digital-upstream.ru>
2023-12-14 14:18:37 +0500

    useParams, useEffect

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -1,31 +1,37 @@
-
-import Profile from './Profile';
-import React from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect } from "react-redux";
+import Profile from "./Profile";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import { setUserProfile } from '../../redux/profile-reducer';
 
-class ProfileContainer extends React.Component {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-            .then(response => {
-                this.props.setUserProfile(response.data);
+function ProfileContainer(props) {
+    let { userId } = useParams();
+    if (!userId) {
+        userId = 2;
+    }
+
+    useEffect(() => {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
+            .then((response) => {
+                props.setUserProfile(response.data);
             });
-    }
+    }, [userId]);
 
-    render() {
-        return (
-            <Profile {...this.props} profile={this.props.profile} />
-        )
-    }
+    return (
+        <div>
+            <Profile profile={props.profile} />
+        </div>
+    );
 }
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
-
 })
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+export default connect(mapStateToProps, { setUserProfile })(ProfileContainer)
+
 
 
